eac8c088a706fb7af8cb7238980b30fbbd574e06
Luna Yao <github@me.zznq.buzz>
2025-06-25 21:37:34 +0200

    Update actions runner

--- Actions-Runner/entrypoint.sh
+++ Actions-Runner/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+set -eux
+
 sudo groupadd -g $DOCKER_GID docker-host
 
 sudo usermod -aG docker-host $(whoami)
 
-exec sg docker-host -c './config.sh --url $RUNNER_URL --token $RUNNER_TOKEN --name $RUNNER_NAME --unattended --replace && ./run.sh
+exec sg docker-host -c "$@"
+
