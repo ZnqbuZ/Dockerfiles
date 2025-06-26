1663128bd91a16df7154c3c6c9bc6bf562bd17b5
Luna Yao <github@me.zznq.buzz>
2025-06-26 22:20:25 +0200

    Set UID as optional

--- Actions-Runner/gen-docker-compose.py
+++ Actions-Runner/gen-docker-compose.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description="Generate a Docker Compose file for
 parser.add_argument("--dir", type=str, default="/var/lib/runners", help="Directory for runners")
 parser.add_argument("--prefix", type=str, help="Prefix for runner names")
 parser.add_argument("--scale", type=int, default=4, help="Number of runners to create")
+parser.add_argument("--uid", type=int, help="UID for the runner user")
 parser.add_argument("--docker-gid", type=int, help="GID for the Docker group on the host")
 parser.add_argument("--url", type=str, help="URL of the GitHub Actions runner")
 parser.add_argument("--token", type=str, help="Token for the GitHub Actions runner")
@@ -17,6 +18,7 @@ runners_dir = args.dir
 runner_group = args.prefix
 scale = args.scale
 
+runner_uid = args.uid
 docker_gid = args.docker_gid
 runner_url = args.url
 runner_token = args.token
@@ -34,9 +36,11 @@ runner = {
         'DOCKER_GID': docker_gid,
         'RUNNER_URL': runner_url,
         'RUNNER_TOKEN': runner_token,
-        'RUNNER_UID': 42000,
     }
 }
+
+if runner_uid is not None:
+    runner['environment']['RUNNER_UID'] = runner_uid
 
 for i in range(scale):
     runner_home = os.path.join(runners_dir, f'{runner_group}-{i}')
