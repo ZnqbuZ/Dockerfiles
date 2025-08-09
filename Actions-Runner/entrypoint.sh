5230a4ff7f6ce0165444f9be205b5d05518c8e59
Luna Yao <github@me.zznq.buzz>
2025-08-09 19:58:46 +0200

    use setup_user

--- Actions-Runner/entrypoint.sh
+++ Actions-Runner/entrypoint.sh
@@ -2,69 +2,25 @@
 
 set -euxo pipefail
 
-USER="runner"
-GROUP="runner"
-
-GID=${GID:-${UID:-}}
-
-if [ -n "${GID:-}" ]; then
-    echo "GID set to $GID."
-    EXISTING_GROUP=$(getent group "$GID" | cut -d: -f1 || true)
-    if [ -n "$EXISTING_GROUP" ]; then
-        GROUP="$EXISTING_GROUP"
-        echo "Using existing group '$GROUP' with GID $GID."
-    else
-        echo "Creating group '$GROUP' with GID $GID."
-        addgroup --gid "$GID" "$GROUP"
-    fi
-else
-    GID=0
-	GROUP="root"
-    echo "GID is not set, using 0."
-fi
-
-if [ -n "${UID:-}" ]; then
-    echo "UID set to $UID."
-    EXISTING_USER=$(getent passwd "$UID" | cut -d: -f1 || true)
-    if [ -n "$EXISTING_USER" ]; then
-        USER="$EXISTING_USER"
-        echo "Using existing user '$USER' with UID $UID."
-    else
-        echo "Creating user '$USER' with UID $UID."
-        adduser --disabled-password --gecos "" --uid "$UID" --gid "$GID" --no-create-home "$USER"
-        usermod -aG sudo "$USER"
-    fi
-else
+if [ -z "$USER" ] && [ -z "$UID" ] && [ -z "$GROUP" ] && [ -z "$GID" ]; then
+    USER=root
     UID=0
-	USER="root"
-    echo "UID is not set, using 0."
+    GROUP=root
+    GID=0
 fi
 
-if [ $UID -eq 0 ]; then
-	echo "Running as root. Setting RUNNER_ALLOW_RUNASROOT to 1."
-	export RUNNER_ALLOW_RUNASROOT=1
-fi
-
-mkdir -p "$HOME"
-
-if [ "$UID" -ne 0 ]; then
-    chown -R "$USER":"$GROUP" "$HOME"
-    usermod -d "$HOME" "$USER"
-fi
-
-groupadd -g $DOCKER_GID -f docker-host
-usermod -aG docker-host $USER
+setup_user --extra-gid "$DOCKER_GID"
 
 if [ -f "$HOME/.credentials" ] && [ -f "$HOME/.credentials_rsaparams" ]; then
 	echo "Credentials found, skip configuring."
 else
 	echo "No credentials found."
 	echo "Installing actions runner..."
-	rsync -a --info=progress2 --delete /runner/ $HOME/
+	rsync -a --info=progress2 --delete /runner/ "$HOME"/
 	echo "Configuring..."
-	gosu $USER $HOME/config.sh --url $RUNNER_URL --token $RUNNER_TOKEN --name $RUNNER_NAME --unattended --replace
+	gosu "$USER" "$HOME"/config.sh --url "$RUNNER_URL" --token "$RUNNER_TOKEN" --name "$RUNNER_NAME" --unattended --replace
 fi
 
-cd $HOME
+cd "$HOME"
 
-exec gosu $USER bash -c "$@"
+exec gosu "$USER" bash -c "$@"
