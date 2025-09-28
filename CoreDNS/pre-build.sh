40fb9a60d2827d762118e9804867892dabac0a4d
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2025-09-28 15:59:18 +0200

    Add libunbound to CoreDNS

--- CoreDNS/pre-build.sh
+++ CoreDNS/pre-build.sh
@@ -16,3 +16,5 @@ mv "$DIR-${LATEST_TAG#v}" "$DIR"
 cd "$DIR" || exit 1
 
 sed -i '/forward:forward/i alternate:github.com/coredns/alternate' plugin.cfg
+echo 'unbound:github.com/coredns/unbound' >> plugin.cfg
+
